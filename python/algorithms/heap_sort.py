1f1e74ba6b01e08aeb7be73f705cdbc4cf23cc1c
oskarmeyenburg <oskar.meyenburg@gmail.com>
2024-04-01 11:46:40 +0200

    Working heap sort

--- python/algorithms/heap_sort.py
+++ python/algorithms/heap_sort.py
@@ -1,77 +1,64 @@
 from algorithms.base_sort import BaseSort
 
 
-class MaxHeapTree:
-    def __init__(self):
-        self.root = None
-
-    def append(self, value):
-        if self.root is None:
-            self.root = Node(value)
-        else:
-            self.root.append(value)
-
-    def heapify(self):
-        if not self.root is None:
-            self.root.heapify()
-
-
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
-
-    def append(self, value):
-        if self.value < value:
-            value, self.value = self.value, value
-
-        if self.left is None:
-            self.left = Node(value)
-        elif self.right is None:
-            self.right = Node(value)
-        elif self.left.value > self.right.value:
-            self.left.append(value)
-        else:
-            self.right.append(value)
-
-    def heapify(self):
-        lnone = self.left is None
-        rnone = self.left is None
-
-        if lnone:
-            if not rnone and self.right.value > self.value:
-                self.value, self.right.value = self.right.value, self.value
-        elif rnone:
-            if self.left.value > self.value:
-                self.value, self.left.value = self.left.value, self.value
-        elif self.left.value > self.right.value:
-            if self.value < self.left.value:
-                self.value, self.left.value = self.left.value, self.value
-        else:
-            if self.value < self.right.value:
-                self.value, self.right.value = self.right.value, self.value
-
-
 class HeapSort(BaseSort):
-    def __init__(self):
-        super().__init__()
-        self.reset()
-
-    def reset(self):
-        self.index = 0
-        self.count = 0
-
-        self.done = False
-        self.highlight_sorting = -1
-        self.highlight_comparing = -1
-        self.highlight_sorted = set()
-
     def sort(self, array):
-        tree = MaxHeapTree()
+        heap = []
+
         for value in array:
-            tree.append(value)
+            i = len(heap)
+            heap.append(value)
+
+            while i:
+                parent = get_parent(i)
+                if heap[parent] < heap[i]:
+                    heap[parent], heap[i] = heap[i], heap[parent]
+                    i = parent
+                else:
+                    break
 
         array.clear()
-        while not (tree.root.left is None and tree.root.right is None):
-            pass
+        while heap:
+            array[:] = [heap[0]] + array
+            heap[0] = heap[-1]
+            heap.pop(-1)
+
+            i = 0
+            l = len(heap)
+
+            if not l:
+                break
+
+            left_child = get_left_child(i)
+            right_child = get_right_child(i)
+            
+            while left_child < l:
+                if right_child >= l:
+                    self.swap(heap, i, left_child)
+                    i = left_child
+                elif heap[left_child] > heap[right_child]:
+                    self.swap(heap, i, left_child)
+                    i = left_child
+                else:
+                    self.swap(heap, i, right_child)
+                    i = right_child
+
+                left_child = get_left_child(i)
+                right_child = get_right_child(i)
+
+            while i != 0:
+                parent = get_parent(i)
+                if heap[parent] < heap[i]:
+                    self.swap(heap, i, parent)
+                    i = parent
+                else:
+                    break
+
+def get_left_child(i):
+    return 2 * i + 1
+
+def get_right_child(i):
+    return 2 * i + 2
+
+def get_parent(i):
+    return (i - 1) // 2
