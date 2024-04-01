295e581a7dfe916650d61ce52d36f1cc8c42cf95
oskarmeyenburg <oskar.meyenburg@gmail.com>
2024-04-01 17:13:59 +0200

    Inplace heap sort

--- python/algorithms/heap_sort.py
+++ python/algorithms/heap_sort.py
@@ -3,56 +3,66 @@ from algorithms.base_sort import BaseSort
 
 class HeapSort(BaseSort):
     def sort(self, array):
-        heap = []
-
-        for value in array:
-            i = len(heap)
-            heap.append(value)
+        for i, value in enumerate(array):
+            array[i] = value
 
             while i:
                 parent = get_parent(i)
-                if heap[parent] < heap[i]:
-                    heap[parent], heap[i] = heap[i], heap[parent]
-                    i = parent
-                else:
+                if array[parent] >= array[i]:
                     break
 
-        array.clear()
-        while heap:
-            array[:] = [heap[0]] + array
-            heap[0] = heap[-1]
-            heap.pop(-1)
+                array[parent], array[i] = array[i], array[parent]
+
+                self.highlight_index = (i, parent)
+                self.wait()
+
+                i = parent
+
+            self.wait()
+
+        heap_size = len(array)
+        while True:
+            heap_size -= 1
+            self.swap(array, 0, heap_size)
+            if heap_size == 0:
+                break
 
             i = 0
-            l = len(heap)
 
-            if not l:
-                break
+            self.highlight_index = (0, heap_size)
+            self.highlight_group = range(heap_size, len(array))
+            self.wait()
 
             left_child = get_left_child(i)
             right_child = get_right_child(i)
             
-            while left_child < l:
-                if right_child >= l:
-                    self.swap(heap, i, left_child)
+            while left_child < heap_size:
+                if right_child >= heap_size:
+                    self.swap(array, i, left_child)
                     i = left_child
-                elif heap[left_child] > heap[right_child]:
-                    self.swap(heap, i, left_child)
+                elif array[left_child] > array[right_child]:
+                    self.swap(array, i, left_child)
                     i = left_child
                 else:
-                    self.swap(heap, i, right_child)
+                    self.swap(array, i, right_child)
                     i = right_child
 
                 left_child = get_left_child(i)
                 right_child = get_right_child(i)
 
+                self.highlight_index = (-1, -1, i,)
+                self.wait()
+            
             while i != 0:
                 parent = get_parent(i)
-                if heap[parent] < heap[i]:
-                    self.swap(heap, i, parent)
-                    i = parent
-                else:
+                if array[parent] >= array[i]:
                     break
+                
+                self.swap(array, i, parent)
+                i = parent
+
+                self.highlight_index = (-1, -1, i,)
+                self.wait()
 
 def get_left_child(i):
     return 2 * i + 1
