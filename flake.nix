45b0da211c537393db87ad9614fcd8b34dbd5efc
omeyemburg <oskar.git@gmx.net>
2025-05-12 14:43:27 +0200

    feat(nix): added flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Python development environment";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs.python312Packages; [
+            pygame-ce
+          ];
+
+          shellHook = ''
+            export SHELL=${pkgs.bashInteractive}/bin/bash
+          '';
+        };
+      }
+    );
+}
